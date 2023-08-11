d61eee2b9d95a916a5733984f90633c5a5a1575c
Elena Vaschenko <galanos.elena@gmail.com>
2023-08-12 03:07:48 +0600

    9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
 # Application definition
 
